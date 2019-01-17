bd2bb01f6cc431e143cecce8e574f2d69a06b6fb
DPRKunicorn <44052116+DPRKunicorn@users.noreply.github.com>
2019-01-17 09:14:53 +0100

    Update Konstrukt.java

--- src/Konstrukt.java
+++ src/Konstrukt.java
@@ -1,7 +1,7 @@
 public class Konstrukt extends Exception
 {
     public Konstrukt(){
-        super();
+        super("Kannnst du echt nicht eine Email schreiben ????? wir sind im Jahre 2019!!!!");
     }
 
     public Konstrukt(String message)
